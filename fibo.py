7fa65a7ba7a1c5782959d6d0eb58c93206cdf125
aaaa <aaaa.python@gmail.com>
2021-02-09 06:11:42 +0900

    module change

--- fibo.py
+++ fibo.py
@@ -1,5 +1,20 @@
 # -*- coding:utf-8 -*-
 
+def fib(n):
+    a, b = 0, 1
+
+    while b < n:
+        print(b, end=' ')
+        a, b = b, a+b
+    print()
+
+def fib2(n):
+    a, b = 0, 1
+    while b < n:
+        result.append(b)
+        a, b = b, a+b
+    return result
+
 if __name__ == "__main__":
     import sys
-    fibo(int(sys.argv[1]))
+    fib(int(sys.argv[1]))
