a6b0c587f91fa2fe23f8013caba331d8248a34a9
aaaa <aaaa.python@gmail.com>
2021-02-09 22:38:03 +0900

    ca Calculation

---
+++ calm.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+print(2 + 2)  #4
+print(50 - 5*6) # 20
+print((50-5*6) / 4) # 5.0
+print(8 / 5) #1.6
+
+print(17 / 3) #5.666666666666667
+print(17 // 3) #5
+print(17 % 3) #2
+print(5*3 + 2) #17
+
+print(5 ** 2) #25
+print(2 ** 7) #128
+
+width = 20
+height = 5*9
+print(width * height)
+
+print(3*3.75 / 1.5) #7.5
+print(7.0 / 2) #3.5
